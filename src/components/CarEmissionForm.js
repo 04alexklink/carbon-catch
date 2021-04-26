a29d6339eb20d0e660f8cd5355749734a7d59f78
04alexklink <alex_klink123@hotmail.co.uk>
2021-04-26 15:57:44 +0100

    Car Emission Form created and makes API call to carbon interface posting vehicle emission data. API call successful

--- src/components/CarEmissionForm.js
+++ src/components/CarEmissionForm.js
@@ -1,9 +1,55 @@
-import React from 'react'
+import React, {useState} from 'react'
+import axios from 'axios'
 
 const CarEmissionForm = () => {
+
+  const initialState = {
+    type: "vehicle",
+    distance_unit: 'miles',
+    distance_value: 0,
+    vehicle_model_id: "7268a9b7-17e8-4c8d-acca-57059252afe9"
+  }
+  const [carFormData, setCarFormData] = useState(initialState)
+
+  const distance = (e) => {
+    setCarFormData({
+      ...carFormData, distance_value: e.target.value
+    })
+    console.log(carFormData)
+  }
+
+  const units = (e) => {
+    setCarFormData({
+      ...carFormData, distance_unit: e.target.value
+    })
+    console.log(carFormData)
+  }
+
+  const submitJourney = async (e) => {
+    e.preventDefault();
+    const config = {
+      headers: {
+        'Authorization': `Bearer ${process.env.REACT_APP_CARBON_INTERFACE_API_KEY}`,
+        'Content-Type': 'application/json'
+      }
+    }
+    const res = await axios.post('https://www.carboninterface.com/api/v1/estimates', carFormData, config)
+    console.log(res, "RESPONSE PLEASE")
+  }
+
   return (
     <div className='CarEmissionForm'>
-      <p>hello</p>
+      <h2>Add Car Journey Info</h2>
+      <form onSubmit={(e)=> submitJourney(e)}>
+      <label htmlFor="distance">Choose Miles or Km's</label>
+      <select id="distance" name="distance" onChange={(e) => units(e)}>
+        <option value="miles">Miles</option>
+        <option value="km">Kilometres</option>
+      </select>
+      <label>Add journey distance:</label>
+      <input type="number" onChange={(e) => distance(e)}></input>
+      <button className="btn">Submit Journey</button>
+      </form>
     </div>
   )
 }
