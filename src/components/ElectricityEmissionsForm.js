2c17ca6a24952815b94cdfae05ed73b3678d5028
Matthew Lock <matthew_lock20@hotmail.com>
2021-04-27 18:06:51 +0100

    Created form in electricityEmissionForm component and added event handlers to form state

--- src/components/ElectricityEmissionsForm.js
+++ src/components/ElectricityEmissionsForm.js
@@ -1,9 +1,43 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const ElectricityEmissionsForm = () => {
+
+  const initialState ={
+    type: "electricity",
+    electricity_unit: '',
+    electricity_value: 0,
+    country: 'us'
+  }
+
+  const [electricityFormData, setElectricityFormData] = useState(initialState)
+
+  const units = (e) => {
+
+    setElectricityFormData({
+      ...electricityFormData, electricity_unit: e.target.value
+    })
+  }
+
+  const electricity = (e) => {
+    setElectricityFormData({
+      ...electricityFormData, electricity_value: parseInt(e.target.value)
+    })
+  }
+
+
   return (
-    <div>
-      <h1>hello</h1>
+      <div className='ElectricityEmissionForm'>
+      <h2>Add Electricity Usage</h2>
+      <form>
+      <label htmlFor="electricity_units">Choose mwh or kwh</label>
+      <select id="electricity_units" name="electricity_units" onChange={(e) => units(e)}>
+        <option value="mwh">Mwh</option>
+        <option value="kwh">Kwh</option>
+      </select>
+      <label>Add Electricity Value:</label>
+      <input type="number" onChange={(e) => electricity(e)}></input>
+      <button className="btn">Submit Usage</button>
+      </form>
     </div>
   )
 }
