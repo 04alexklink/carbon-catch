80a7a74cca99b672361f702ba53cdf8f9f5d62ad
04alexklink <alex_klink123@hotmail.co.uk>
2021-04-27 13:27:23 +0100

    Images folder moved into src. Car image now loads correctly in EmissionTypesContainer component

--- src/components/Car.js
+++ src/components/Car.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import carImage from '../images/car-emoji.jpeg'
 
 const Car = ({showCarForm}) => {
     console.log('In Car Component', {showCarForm})
     return (
         <div>
             <h1>Car CO2</h1>
-           <img src="./../../images/car-emoji.jpeg" alt="image-of-car"></img>
+           <img src={carImage} width="100px" height="100px" alt="image-of-car"></img>
            <br></br>
            <button onClick={() => showCarForm()}>Calculate Car Journey Emissions</button> 
         </div>
