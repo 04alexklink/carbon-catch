1b9ade89c08cffb953e2b723083c2c475ab0550f
04alexklink <alex_klink123@hotmail.co.uk>
2021-05-01 20:56:42 +0100

    Fixed Bar Chart showing users annual emissions comapred to average uk emissison in tonnes. Y-axis now has correct label too.

--- src/components/TotalBarChart.js
+++ src/components/TotalBarChart.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
+import { BarChart, Bar, XAxis, YAxis, Label, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
 
 const TotalBarChart = ({userEmissions}) => {
@@ -13,25 +13,39 @@ const TotalBarChart = ({userEmissions}) => {
 
   return (
     <div className="totalbarchart">
+      <ResponsiveContainer width={"100%"} height={400}>
       <BarChart
       width={500}
       height={300}
       data={data}
       margin={{
-        top: 5,
+        top: 30,
         right: 30,
-        left: 20,
-        bottom: 5
+        left: 30,
+        bottom: 30
       }}
     >
-      <CartesianGrid strokeDasharray="3 3" />
-      <XAxis dataKey="name" />
-      <YAxis />
+      <CartesianGrid />
+      <XAxis dataKey="name">
+      <Label 
+      position="bottom"
+      style={{textAnchor: "middle"}}
+      />
+      </XAxis>
+      <YAxis>
+        <Label 
+        value={"Carbon Emissions (kg)"}
+        position="left"
+        angle={-90}
+        style={{textAnchor: "middle"}}
+        />
+      </YAxis>
       <Tooltip />
       <Legend />
       <Bar dataKey="Avg_emissions_pp" fill="#8884d8" />
       <Bar dataKey="Your_emissions" fill="#82ca9d" />
     </BarChart>
+    </ResponsiveContainer>
     </div>
   )
 }
