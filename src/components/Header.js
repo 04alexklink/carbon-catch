761fbb16228d3d5a05f815b69932feac2b198861
Matthew Lock <matthew_lock20@hotmail.com>
2021-05-21 18:01:05 +0100

    Changed favicon and added wind image

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,10 @@
+import wind from "../images/wind.png"
+
 
 const Header = () => (
-  <div className="logo">Carbon Catch</div>
+  <div className="logo">Carbon Catch
+  <img src={wind} alt="" width="30px" height="30px"></img>
+  </div>
 )
 
 export default Header
